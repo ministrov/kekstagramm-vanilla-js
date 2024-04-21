68fbd4a7ec25c3e7904934ed5d5f6dc08b5448d8
ministrov <61511030+ministrov@users.noreply.github.com>
2024-04-21 18:09:44 +0300

    feat: practicing writing basic types of javascript and declare some variables with let, const

--- js/main.js
+++ js/main.js
@@ -2,5 +2,25 @@
 
 let name = 'Anton';
 
+// console.log(name);
+
 window.console.log(name);
+
+const age = 45;
+
+window.console.log(typeof (age + name));
+
+function sumNumers(first, second) {
+  return first + second;
+}
+
+const result = sumNumers(3, 6);
+
+console.log(result);
+
+console.log(sumNumers(2, 5));
+
+// const MY_GENDER = 'male';
+// const EARTH_ROUND = 40000;
+
 
