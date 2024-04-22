9acd2cf5eb2bbd89132c03549df735bfb49e8f8f
ministrov <61511030+ministrov@users.noreply.github.com>
2024-04-22 22:03:09 +0300

    feat: opating on module2 task1 write two helper functions

--- js/main.js
+++ js/main.js
@@ -1,35 +1,86 @@
-// import random from "./helpers";
-
+/**
+ *
+ * @param {Number} min From minimun number
+ * @param {Number} max To maximum number
+ * @returns {Number} random number in range from min to max
+ */
 const getRandomRange = function(min, max) {
+  if (min < 0 && max < 0) return;
+
+  if (max < min) return;
+
   return Math.floor(Math.random() * (min - max + 1)) + min;
 }
 
-const random = Math.floor(Math.random() * 11);
+/**
+ *
+ * @param {String} str The string to check
+ * @param {Number} length Max length of the string
+ * @returns {Boolean} true or false
+*/
+const checkStrLength = function(str, length = 50) {
+  let total;
+  if (str.length < length) {
+    total = false;
+  } else {
+    total = true;
+  }
 
-console.log(random);
+  return total;
+}
 
-console.log(getRandomRange(4, 9));
-// let name = 'Anton';
+/**
+ *
+ * @param {String} first The first name
+ * @param {String} last  The last name
+ * @param {Object} [params={}] Additional parameters
+ * @param {Number} [params.age] Optional age
+ * @param {String} [params.city] Optional city
+ */
+const textFunction = (first, last, params = {}) => {
+  const { age = 22, city = 'Medelin' } = params;
 
-// // console.log(name);
+  console.log(`The name is ${first} ${last}`);
+  console.log(`The age is ${age} and the city ${city}`)
+}
 
-// // window.console.log(name);
+console.log(textFunction('Anton', 'Zhilin'));
 
-// const age = 45;
 
-// window.console.log(typeof (age + name));
 
-// function sumNumers(first, second) {
-//   return first + second;
-// }
+// const random = Math.floor(Math.random() * 11);
 
-// const result = sumNumers(3, 6);
+// console.log(random);
+
+// console.log(getRandomRange(4, 9));
+
+// const bookCount = 19;
+
+// let result;
+
+// result = (bookCount > 15) ? 'План на год выполнен!' : 'Читать и ещё раз читать';
 
 // console.log(result);
 
-// console.log(sumNumers(2, 5));
+// function toString(value) {
+//   return '' + value;
+// }
 
-// // const MY_GENDER = 'male';
-// // const EARTH_ROUND = 40000;
+// console.log(toString(4));
+// console.log(typeof toString(4));
 
+// function toNumber(value) {
+//   return +value;
+// }
 
+// const pow = function(base, exponent) {
+//   let total = base;
+
+//   for (let i = 1; i < exponent; i++) {
+//     total *= base;
+//   }
+
+//   return total;
+// }
+
+// console.log(pow(2, 2));
