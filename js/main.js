4a496df29a6fd9958d51b319433c97dd4dfb780a
ministrov <61511030+ministrov@users.noreply.github.com>
2024-04-24 14:02:26 +0300

    feat: starts creating  functions in module3-task1

--- js/main.js
+++ js/main.js
@@ -30,27 +30,61 @@ const checkStrLength = function(str, length) {
 
 console.log(checkStrLength('Mynamedisdf', 12));
 
-console.log(NaN === NaN);
-console.log(typeof NaN);
-
-const obj = {
-  a: 1,
-  b: 'какая-то строка'
-};
-
-console.log(obj.a);
-console.log(obj['b']);
-
-const house = {
-  squareMeters: 145,
-  floorCount: 2,
-  numberOfBedroom: 2
+/**
+ * This is a basic closure function
+ * @returns {Number} count is number
+ */
+function createCounter() {
+  let count = 0; // Приватная переменная, доступная только внутри createCounter
+  return function () {
+    count += 1;
+    return count;
+  };
 }
 
-console.log(house.floorCount);
-console.log(house['floorCount']);
-console.log((+'5') + (+'3'));
+const counter = createCounter(); // counter — это функция, замыкающая в себе переменную count
+console.log(counter()); // 1
+console.log(counter()); // 2
 
-console.log(0 && '');
+const b = 2;
 
-console.log(0 || 4);
+function getNumber(number) {
+  return number * 10;
+}
+
+console.log(getNumber(b));
+
+const obj = {
+  name: 'Anton',
+  date: '25/05/1983',
+  job: 'Freelancer',
+  isMaried: false,
+  hasBrother: true
+}
+
+
+/**
+ *The function generates 25 objects of user's photos
+ * @returns {Array} Array of 25 generated objects
+ */
+const generateArrayOfPhotos = function() {
+  return console.log(['1', '2', '3', 4, false])
+}
+/**
+ * This function generate a classic objec of some photo creating by user
+ * @param {Object} data
+ * @returns {Object} Object with data to use
+ */
+const generateObject = function(data) {
+  return {
+    id: data.id,
+    url: data.id,
+    description: data.description,
+    likes: data.likes,
+    comments: [],
+    avatar: `img/avatar-${getRandomRange(1, 6)}.svg`,
+    message: ['Всё отлично!', 'В целом всё неплохо.Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.', 'В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают.Как можно было поймать такой неудачный момент ? !']
+  }
+}
+
+
