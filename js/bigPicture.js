4efe11e1e22c1a230e782d51d02cfe0968e71e3a
unknown <ministrov2018@gmail.com>
2024-05-02 16:46:19 +0300

    feat: end up operating on bigPicture feature realization

--- js/bigPicture.js
+++ js/bigPicture.js
@@ -1,1 +1,54 @@
 const bigPicture = document.querySelector('.big-picture');
+const body = document.querySelector('body');
+const bigPictureClose = bigPicture.querySelector('.big-picture__cancel');
+
+// Just hide
+const commentsCount = bigPicture.querySelector('.social__comment-count');
+const commentsLoader = bigPicture.querySelector('.social__comments-loader');
+commentsCount.classList.add('hidden');
+commentsLoader.classList.add('hidden');
+
+const onBigPictureCloseClick = () => {
+  bigPicture.classList.add('hidden');
+  body.classList.remove('modal-open');
+  bigPictureClose.removeEventListener('click', onBigPictureCloseClick);
+  commentsList.innerHTML = '';
+}
+
+// The function for inputing comments
+
+const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
+const commentsList = document.querySelector('.social__comments');
+
+const renderComment = (comment) => {
+  const commentClone = commentTemplate.cloneNode(true);
+
+  commentClone.querySelector('.social__picture').src = comment.avatar;
+  commentClone.querySelector('.social__picture').alt = comment.name;
+  commentClone.querySelector('.social__text').textContent = comment.message;
+
+  return commentClone;
+}
+
+const renderComments = (comments) => {
+  let commentsListFragment = document.createDocumentFragment();
+
+  comments.forEach((comment) => commentsListFragment.appendChild(renderComment(comment)));
+
+  commentsList.appendChild(commentsListFragment)
+}
+
+const showBigPicture = (picture) => {
+  body.classList.add('modal-open');
+  bigPicture.querySelector('.big-picture__img > img').src = picture.url;
+  bigPicture.querySelector('.likes-count').textContent = picture.likes;
+  bigPicture.querySelector('.comments-count').textContent = picture.comments.length;
+  bigPicture.querySelector('.social__caption').textContent = picture.description;
+
+  bigPictureClose.addEventListener('click', onBigPictureCloseClick);
+
+  bigPicture.classList.remove('hidden');
+  renderComments(picture.comments);
+}
+
+export { showBigPicture }
--- js/userPicture.js
+++ js/userPicture.js
@@ -1,13 +1,19 @@
 import { photos } from "./data.js";
+import { showBigPicture } from "./bigPicture.js";
 
 const picturesList = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const renderPhoto = ({ url, comments, likes }) => {
+const renderPhoto = (picture) => {
   const photoPreview = pictureTemplate.cloneNode(true);
-  photoPreview.querySelector('.picture__img').src = url;
-  photoPreview.querySelector('.picture__likes').textContent = likes;
-  photoPreview.querySelector('.picture__comments').textContent = comments.length;
+  photoPreview.querySelector('.picture__img').src = picture.url;
+  photoPreview.querySelector('.picture__likes').textContent = picture.likes;
+  photoPreview.querySelector('.picture__comments').textContent = picture.comments.length;
+
+  photoPreview.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    showBigPicture(picture);
+  });
 
   return photoPreview;
 }
