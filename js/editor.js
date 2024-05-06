2c97968d26ab903fa52ae108e47c425fc208fb65
unknown <ministrov2018@gmail.com>
2024-05-06 11:37:19 +0300

    feat: create showing modal after uploading user photo and start creating effects

--- js/editor.js
+++ js/editor.js
@@ -1,4 +1,10 @@
-import { isEscKey } from "./utils";
+import { isEscKey } from "./utils.js";
+
+const Scale = {
+  MAX: 100,
+  MIN: 25,
+  STEP: 25
+}
 
 const body = document.documentElement;
 const uploadModal = document.querySelector('.img-upload__overlay');
@@ -28,4 +34,45 @@ document.addEventListener('keydown', (evt) => {
   if (isEscKey(evt)) {
     closeModal();
   }
+});
+
+// Zooming uploaded picture
+const buttonPlus = uploadModal.querySelector('.scale__control--bigger');
+const buttonMinus = uploadModal.querySelector('.scale__control--smaller');
+const scaleValue = uploadModal.querySelector('.scale__control--value');
+const imagePreview = uploadModal.querySelector('.img-upload__preview > img');
+
+const resetSettings = () => {
+  imagePreview.style = 'transform: scale(1.00)';
+  imagePreview.style.filter = '';
+  if (lastClass) {
+    imagePreview.classList.remove(lastClass);
+  }
+
+  scaleValue.value = '100%';
+  effectLevel.classList.add('visually-hidden');
+}
+
+buttonPlus.addEventListener('click', () => {
+  let scale = parseInt(scaleValue.value, 10) + Scale.STEP;
+
+  if (scale >= Scale.MAX) {
+    scale = Scale.MAX;
+  }
+
+  scaleValue.value = scale + '%';
+  scale = scale / 100;
+  imagePreview.style.transform = 'scale(' + scale + ')';
+});
+
+buttonMinus.addEventListener('click', () => {
+  let scale = parseInt(scaleValue.value, 10) - Scale.STEP;
+
+  if (scale <= Step.MIN) {
+    scale = Step.MIN;
+  }
+
+  scaleValue.value = scale + '%';
+  scale = scale / 100;
+  imagePreview.style.transform = 'scale(' + scale + ')';
 });
---
+++ js/effects.js
@@ -0,0 +1,5 @@
+const Slider = {
+  MAX: 100,
+  MIN: 0,
+  STEP: 1
+}
